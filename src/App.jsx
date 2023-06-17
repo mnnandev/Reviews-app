4eac85f68ab187705b7096a66d6c0d5b097606f2
Mnnan Mazhar <mnnanhavibhutta@gmail.com>
2023-06-17 15:03:19 +0500

    Reviews app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import Review from "./Review";
+
+function App() {
+
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
